037e02f2e754e4f194eed109bf5c6d08f6d71ee3
nercblue <nericblue@gmail.com>
2023-01-17 14:48:11 +0200

    Final

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,13 +3,13 @@
 """
 Created on Mon Aug 24 07:02:53 2020
 
-@author: Robinson Montes
+@author: 
 """
 from requests import get
 
 
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
+    url = 'https://alx-intranet.hbtn.io/status'
     response = get(url)
     bytes_content = response.text
     string = 'Body response:\n\t- type: {}\n\t- content: {}'.format(
